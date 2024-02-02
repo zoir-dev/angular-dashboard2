8426157f17309bf229414abae7d533399d873880
Zoirjon <zoirjonabduvohidov0@gmail.com>
2024-02-02 09:05:20 +0500

    errors edited

--- src/app/shared/services/theme/theme.service.ts
+++ src/app/shared/services/theme/theme.service.ts
@@ -6,7 +6,7 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class ThemeService implements OnInit {
-  darkMode: boolean = false
+  darkMode: boolean = true
 
   darkMode$ = new BehaviorSubject<boolean>(this.darkMode)
   constructor(@Inject(DOCUMENT) private doc:Document) {
